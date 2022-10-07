c4891b2b334ac1ba73481b5723c2951760a2b241
glautrou <g.lautrou@outlook.com>
2022-10-07 22:42:28 +0200

    Clean test type

--- bs-back/dist/src/api/test-type/controllers/test-type.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-/**
- * test-type controller
- */
-Object.defineProperty(exports, "__esModule", { value: true });
-const strapi_1 = require("@strapi/strapi");
-exports.default = strapi_1.factories.createCoreController('api::test-type.test-type');
--- bs-back/src/api/test-type/controllers/test-type.ts
+++
@@ -1,7 +0,0 @@
-/**
- * test-type controller
- */
-
-import { factories } from '@strapi/strapi'
-
-export default factories.createCoreController('api::test-type.test-type');
